12feb8d179b33d8c1ed22e74eefb6e7a0c8d9228
Ammar <ammarwafiyadnan@gmail.com>
2023-05-09 01:33:24 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace MONEYCHANGER_RM_testExercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTotalAmountRM = new System.Windows.Forms.Label();
+            this.textBoxTotalRM = new System.Windows.Forms.TextBox();
+            this.buttonDenominate = new System.Windows.Forms.Button();
+            this.labelmoneyChanger = new System.Windows.Forms.Label();
+            this.listBoxSeperateAmount = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTotalAmountRM
+            // 
+            this.labelTotalAmountRM.AutoSize = true;
+            this.labelTotalAmountRM.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotalAmountRM.Location = new System.Drawing.Point(44, 89);
+            this.labelTotalAmountRM.Name = "labelTotalAmountRM";
+            this.labelTotalAmountRM.Size = new System.Drawing.Size(143, 16);
+            this.labelTotalAmountRM.TabIndex = 0;
+            this.labelTotalAmountRM.Text = "Total Amount (RM) :";
+            // 
+            // textBoxTotalRM
+            // 
+            this.textBoxTotalRM.Location = new System.Drawing.Point(194, 89);
+            this.textBoxTotalRM.Name = "textBoxTotalRM";
+            this.textBoxTotalRM.Size = new System.Drawing.Size(135, 22);
+            this.textBoxTotalRM.TabIndex = 1;
+            // 
+            // buttonDenominate
+            // 
+            this.buttonDenominate.Location = new System.Drawing.Point(358, 87);
+            this.buttonDenominate.Name = "buttonDenominate";
+            this.buttonDenominate.Size = new System.Drawing.Size(150, 65);
+            this.buttonDenominate.TabIndex = 3;
+            this.buttonDenominate.Text = "DENOMINATE";
+            this.buttonDenominate.UseVisualStyleBackColor = true;
+            this.buttonDenominate.Click += new System.EventHandler(this.buttonDenominate_Click);
+            // 
+            // labelmoneyChanger
+            // 
+            this.labelmoneyChanger.AutoSize = true;
+            this.labelmoneyChanger.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelmoneyChanger.Location = new System.Drawing.Point(333, 29);
+            this.labelmoneyChanger.Name = "labelmoneyChanger";
+            this.labelmoneyChanger.Size = new System.Drawing.Size(143, 16);
+            this.labelmoneyChanger.TabIndex = 4;
+            this.labelmoneyChanger.Text = "MONEY CHANGER ";
+            // 
+            // listBoxSeperateAmount
+            // 
+            this.listBoxSeperateAmount.FormattingEnabled = true;
+            this.listBoxSeperateAmount.ItemHeight = 16;
+            this.listBoxSeperateAmount.Location = new System.Drawing.Point(33, 183);
+            this.listBoxSeperateAmount.Name = "listBoxSeperateAmount";
+            this.listBoxSeperateAmount.Size = new System.Drawing.Size(694, 244);
+            this.listBoxSeperateAmount.TabIndex = 5;
+            this.listBoxSeperateAmount.SelectedIndexChanged += new System.EventHandler(this.listBoxSeperateAmount_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(676, 353);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 16);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(528, 353);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(11, 16);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(357, 353);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(11, 16);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(207, 353);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(11, 16);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(58, 353);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(11, 16);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "-";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(676, 333);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(38, 16);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "RM 1";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(528, 333);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(38, 16);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "RM 5";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(356, 333);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(45, 16);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "RM 10";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(206, 333);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(45, 16);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "RM 20";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(55, 333);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(45, 16);
+            this.label15.TabIndex = 20;
+            this.label15.Text = "RM 50";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(515, 89);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(151, 63);
+            this.buttonClear.TabIndex = 30;
+            this.buttonClear.Text = "CLEAR";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.listBoxSeperateAmount);
+            this.Controls.Add(this.labelmoneyChanger);
+            this.Controls.Add(this.buttonDenominate);
+            this.Controls.Add(this.textBoxTotalRM);
+            this.Controls.Add(this.labelTotalAmountRM);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTotalAmountRM;
+        private System.Windows.Forms.TextBox textBoxTotalRM;
+        private System.Windows.Forms.Button buttonDenominate;
+        private System.Windows.Forms.Label labelmoneyChanger;
+        private System.Windows.Forms.ListBox listBoxSeperateAmount;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MONEYCHANGER_RM_testExercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+
+        private void buttonDenominate_Click(object sender, EventArgs e)
+        {
+            int totalAmount = int.Parse(textBoxTotalRM.Text);
+            int rm50Count = totalAmount / 50;
+            int rm20Count = (totalAmount % 50) / 20;
+            int rm10Count = ((totalAmount % 50) % 20) / 10;
+            int rm5Count = (((totalAmount % 50) % 20) % 10) / 5;
+            int rm1Count = (((totalAmount % 50) % 20) % 10) % 5;
+
+            label10.Text = rm50Count.ToString();
+            label9.Text = rm20Count.ToString();
+            label8.Text = rm10Count.ToString();
+            label7.Text = rm5Count.ToString();
+            label6.Text = rm1Count.ToString();
+
+
+            /*int[] denominations = { 50, 20, 10, 5, 1 };
+            int numDenominations = 5;
+
+            //int rm50, rm20, rm10, rm5, rm1;
+
+            int[]count = new int[5];
+
+            for (int i = 0; i < numDenominations; i++)
+            {
+                count[i] = amount / denominations[i];
+                amount %= denominations[i];
+                listBoxSeperateAmount.Items.Add($"RM{denominations[i]} x {count[i]}\n");
+     
+    
+
+            }*/
+            listBoxSeperateAmount.Items.Add("\n");
+        }
+
+        private void listBoxSeperateAmount_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            label10.Text = "";
+            label9.Text = "";
+            label8.Text = "";
+            label7.Text = "";
+            label6.Text = "";
+
+
+            /*foreach (Control control in Controls)
+            {
+                if (control is TextBox textBox)
+                {
+                    textBox.Clear(); // Clear all text boxes
+                }
+                else if (control is ListBox listBox)
+                {
+                    listBox.Items.Clear(); // Clear all list boxes
+                }
+            }*/
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
